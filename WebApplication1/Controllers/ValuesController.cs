1a0f896eef356c9a496882e836bb2194fd83a102
Alexander Bleissem <alexander.bleissem@ntw-software.com>
2019-02-23 23:27:17 +0100

    added example of working update renamed actions

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -20,8 +20,8 @@ namespace WebApplication1.Controllers
     public class ValuesController : ControllerBase
     {
         // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get([FromServices] ICrudServices service, [FromServices] MyDBContext context, [FromServices] IConfiguration config)
+        [HttpGet("[action]")]
+        public ActionResult<IEnumerable<string>> DoesNotWork([FromServices] ICrudServices service, [FromServices] MyDBContext context, [FromServices] IConfiguration config)
         {
             //first use Dapper to insert data so only an update is done by ICrudServices
             int contactAddressId;
@@ -64,6 +64,47 @@ namespace WebApplication1.Controllers
 
             return new string[] { "value1", "value2" };
         }
-       
+
+        [HttpGet("[action]")]
+        public ActionResult<IEnumerable<string>> WorksGreat([FromServices] ICrudServices service, [FromServices] MyDBContext context, [FromServices] IConfiguration config)
+        {
+            context.AddressNotOwned.RemoveRange(context.AddressNotOwned.ToList());
+            context.ContactAddress.RemoveRange(context.ContactAddress.ToList());
+            context.SaveChanges();
+
+            //ATTEMPT
+            var dto = new InContactAddressDto
+            {
+                Name = "test",
+                Addess = new InAddressDto
+                {
+                    Address1 = "some street"
+                }
+            };
+            service.CreateAndSave(dto);
+
+            //VERIFY
+            service.IsValid.ShouldBeTrue(service.GetAllErrors());
+            var contact = context.ContactAddress.Include(i => i.AddressNotOwned).SingleOrDefault();
+            contact.Name.ShouldEqual("test");
+            contact.AddressNotOwned.Address1.ShouldEqual("some street");
+
+            InContactAddressDto updateDTO = service.ReadSingle<InContactAddressDto>(w => w.Name == "test");
+
+            updateDTO.Name = "test2";
+            updateDTO.Addess.Address1 = "other street";
+
+            service.UpdateAndSave(updateDTO);
+
+            //VERIFY
+            service.IsValid.ShouldBeTrue(service.GetAllErrors());
+            contact = context.ContactAddress.SingleOrDefault();
+            contact.Name.ShouldEqual("test2");
+            contact.AddressNotOwned.Address1.ShouldEqual("other street");
+
+
+            return new string[] { "value1", "value2" };
+        }
+
     }
 }
